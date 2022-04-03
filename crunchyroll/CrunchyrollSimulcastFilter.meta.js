772615a29ad777d2b636151a9d4a37f35f47f227
Kevin Powell <shatterpoint2000@gmail.com>
2022-04-03 13:45:10 -0400

    Initial Commit
    Adding tampermonkey script to hide all dubs releases from the simulcast release calendar of crunchyroll.

---
+++ crunchyroll/CrunchyrollSimulcastFilter.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Crunchyroll Simulcast Filter
+// @namespace    https://github.com/leonidasIIV/TamperMonkey-Scripts/crunchyroll
+// @version      0.1
+// @description  filters out dubs items from the crunchyroll simulcast calendar
+// @author       LeonidasIIV
+// @match        https://www.crunchyroll.com/simulcastcalendar*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=crunchyroll.com
+// @grant        none
+// @updateURL    https://github.com/leonidasIIV/TamperMonkey-Scripts/raw/master/crunchyroll/CruncyrollSimulcastFilter.meta.js
+// @downloadURL  https://github.com/leonidasIIV/TamperMonkey-Scripts/raw/master/crunchyroll/CruncyrollSimulcastFilter.user.js
+// ==/UserScript==
